197df41391c4c12f3060a056b4caa5cfa4e4a411
gitboyzorro5 <kibetleonard8@gmail.com>
2019-01-03 14:49:05 +0300

    updated updatefixtures.sh

--- updatefixtures.sh
+++ updatefixtures.sh
@@ -1,4 +1,4 @@
 #! /bin/bash 
 sed -i '1,$d' fixtures.sql
-awk -F, -f in_fixtures.awk ~/SQLITE/fixtures.csv | sed '1d'| sed "s/'Nott'm Forest'/'Nottm Forest'/g" | sed "s/'M'gladbach'/'Mgladbach'/g" >> ~/SQLITE/fixtures.sql	
+awk -F, -f in_fixtures.awk ~/FDAS/fixtures.csv | sed '1d'| sed "s/'Nott'm Forest'/'Nottm Forest'/g" | sed "s/'M'gladbach'/'Mgladbach'/g" >> ~/FDAS/fixtures.sql	
 
