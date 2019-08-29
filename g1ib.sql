906275879b1dd33e342842e77c6b4d6d65cce56b
gitboyzorro5 <kibetleonard8@gmail.com>
2019-08-29 17:07:34 +0000

    updated awk files to include HTR

--- g1ib.sql
+++ g1ib.sql
@@ -1,7 +1,7 @@
-INSERT INTO G1 VALUES('G1','24/08/2019','Lamia','Panathinaikos','1','1','D','1','1','4.21','3.19','2.05');
-INSERT INTO G1 VALUES('G1','24/08/2019','Aris','OFI Crete','1','1','D','0','1','1.47','3.93','8.99');
-INSERT INTO G1 VALUES('G1','24/08/2019','Olympiacos Piraeus','Asteras Tripolis','1','0','H','1','0','1.23','6.17','13.45');
-INSERT INTO G1 VALUES('G1','25/08/2019','Atromitos','Larisa','1','1','D','1','1','1.66','3.51','6.38');
-INSERT INTO G1 VALUES('G1','25/08/2019','Panionios','Volos NFC','1','2','A','0','1','2.41','3.14','3.26');
-INSERT INTO G1 VALUES('G1','25/08/2019','PAOK','Panetolikos','2','1','H','1','0','1.18','6.81','19.06');
-INSERT INTO G1 VALUES('G1','25/08/2019','AEK','Xanthi','1','2','A','0','0','1.34','4.53','12.58');
+INSERT INTO G1 VALUES('G1','24/08/2019','Lamia','Panathinaikos','1','1','D','1','1','D','4.21','3.19','2.05');
+INSERT INTO G1 VALUES('G1','24/08/2019','Aris','OFI Crete','1','1','D','0','1','A','1.47','3.93','8.99');
+INSERT INTO G1 VALUES('G1','24/08/2019','Olympiacos Piraeus','Asteras Tripolis','1','0','H','1','0','H','1.23','6.17','13.45');
+INSERT INTO G1 VALUES('G1','25/08/2019','Atromitos','Larisa','1','1','D','1','1','D','1.66','3.51','6.38');
+INSERT INTO G1 VALUES('G1','25/08/2019','Panionios','Volos NFC','1','2','A','0','1','A','2.41','3.14','3.26');
+INSERT INTO G1 VALUES('G1','25/08/2019','PAOK','Panetolikos','2','1','H','1','0','H','1.18','6.81','19.06');
+INSERT INTO G1 VALUES('G1','25/08/2019','AEK','Xanthi','1','2','A','0','0','D','1.34','4.53','12.58');
