e1a3263db907fa6638105d5fdc7b7a0578ed1db0
gitboyzorro5 <kibetleonard8@gmail.com>
2020-12-11 03:51:30 +0000

    added shots on target

--- updatematches.sh
+++ updatematches.sh
@@ -18,7 +18,10 @@ sed -f script.vim INSERTALL.sql > tempallmatches0.txt
 awk -v OFS=, -f magical.awk tempallmatches0.txt htcs.txt > tempallmatches1.txt
 awk -v OFS=, -f magical.awk tempallmatches1.txt cs.txt > tempallmatches2.txt
  echo "Div,Date,HomeTeam,AwayTeam,FTHG,FTAG,FTR,HTHG,HTAG,HTR,PSH,PSD,PSA,HTCS,CS" > allteams20202021.csv
-cat tempallmatches2.txt >> allteams20202021.csv
+#cat tempallmatches2.txt >> allteams20202021.csv
+cut -d, -f1,1,2,3,4,5,6,7,8,9,10,11,12,13,18,19 tempallmatches2.txt >> allteams20202021.csv
+ echo "Div,Date,HomeTeam,AwayTeam,FTHG,FTAG,FTR,HTHG,HTAG,HTR,PSH,PSD,PSA,HTCS,CS,HS,HST,AS,AST" > allteams20202021SOT.csv
+awk -F, '{printf("'%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s'\n",$1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$18,$19,$14,$15,$16,$17)}' tempallmatches2.txt >> allteams20202021SOT.csv
 cat allteams2010-present.backup >> allteams2010-present.csv
 sed -n '2,$p' allteams20202021.csv >> allteams2010-present.csv
 sed -i -e 's/\x92//g' EC.csv
