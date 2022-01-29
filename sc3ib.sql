8fdb8f61a7f6b76a10d0f434d4bc27b7daf66c14
gitboyzorro5 <kibetleonard8@gmail.com>
2022-01-29 16:09:15 +0000

    29/01/2022 update

--- sc3ib.sql
+++ sc3ib.sql
@@ -101,3 +101,4 @@ INSERT INTO SC3 VALUES('SC3','15/01/2022','Kelty Hearts','Edinburgh City','2','2
 INSERT INTO SC3 VALUES('SC3','15/01/2022','Stenhousemuir','Cowdenbeath','0','2','A','0','0','D','1.76','3.72','4.38','13','2','12','6','3','2','0','0');
 INSERT INTO SC3 VALUES('SC3','15/01/2022','Stranraer','Stirling','3','3','D','1','1','D','2.3','3.57','2.86','14','8','10','4','3','1','0','0');
 INSERT INTO SC3 VALUES('SC3','22/01/2022','Cowdenbeath','Stranraer','0','1','A','0','0','D','2.58','3.2','2.75','4','2','7','4','2','2','0','0');
+INSERT INTO SC3 VALUES('SC3','25/01/2022','Elgin','Kelty Hearts','2','0','H','1','0','H','4.31','3.75','1.77','6','4','6','3','3','4','0','0');
